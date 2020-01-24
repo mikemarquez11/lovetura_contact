4744946128e2ee57c89cc2fd9dd8f7f868ce2c9e
Mike Marquez <mike.marquez11@gmail.com>
2020-01-24 11:29:59 -0600

    Adding Admin submenu Page

--- admin/lovetura-admin.php
+++ admin/lovetura-admin.php
@@ -14,10 +14,31 @@ namespace LoveturaContact\Admin;
   class LoveturaAdmin {
 
     protected $page_slug = 'contact-page';
+    protected $parent_slug = 'lovetura-contact';
+    protected $lovedate_slug = 'lovetura-date';
 
     public function lovetura_admin_menu() {
 
         add_menu_page(
+            __( 'Lovetura Contact', 'lovetura-contact' ),
+            __( 'Lovetura Settings', 'lovetura-contact' ),
+            'manage_options',
+            $this->parent_slug,
+            [ $this, 'admin_contact_contents' ],
+            'dashicons-heart',
+            6
+            );
+        
+        add_submenu_page(
+            $this->parent_slug, 
+            'Lovetura Dates', 
+            'Lovetura Dates', 
+            'manage_options',
+            $this->lovedate_slug,
+            [ $this, 'admin_lovedate_contents' ],
+        );
+
+        /*add_menu_page(
             __( 'Contact', 'lovetura-contact' ),
             __( 'Contact', 'lovetura-contact' ),
             'manage_options',
@@ -25,7 +46,7 @@ namespace LoveturaContact\Admin;
             [ $this, 'admin_contact_contents' ],
             'dashicons-email-alt',
             6
-            );
+            );*/
     }
 
     public function get_content_page() {
@@ -41,9 +62,14 @@ namespace LoveturaContact\Admin;
         include_once LOVETURACONTACT_PATH . 'views/page-lovetura.php';
     }
 
+    public function admin_lovedate_contents() {
+        include_once LOVETURACONTACT_PATH . 'views/page-lovedate.php';
+    }
+
     public function register_admin_lovescripts() {
         // repeater Script
         wp_register_script( 'lc-repeater', LOVETURACONTACT_URL . 'assets/js/repeater.js' , array( 'jquery' ), LOVETURACONTACT_VERSION, true );
+        wp_register_script( 'ld-repeater', LOVETURACONTACT_URL . 'assets/js/date-repeater.js' , array( 'jquery' ), LOVETURACONTACT_VERSION, true );
     }
 
 
@@ -51,12 +77,12 @@ namespace LoveturaContact\Admin;
 
     //Load only on ?page=sample-page
     
-    if( $hook != 'toplevel_page_contact-page' ) {
+    if( $hook != 'toplevel_page_lovetura-contact' && $hook != 'lovetura-settings_page_lovetura-date' ) {
         return;         
     }
     
-    // Load style & scripts.
-    wp_enqueue_script( 'lc-repeater' );
+        // Load style & scripts.
+        wp_enqueue_script( 'lc-repeater' );
     }
 
     public function setup_fields() {
@@ -70,6 +96,10 @@ namespace LoveturaContact\Admin;
     public function register_options() {
         register_setting('lovecontact_section', 'lovetura_email');
         register_setting('lovecontact_section', 'sesiones');
+    }
+
+    public function register_date_options() {
+        register_setting('lovedate_section', 'unadates');
     }
 
     public function section_callback( $arguments ) {
---
+++ views/page-lovedate.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Admin Page View
+ *
+ * Template
+ */
+$unadates = get_option('unadates');
+?>
+<div class="wrap">
+<style>
+#addDate {
+    font-size: 20px;
+    font-weight: 800;
+}
+
+#remDate {
+    padding: 0 10px;
+    font-size: 15px;
+    font-weight: 800;
+}
+</style>
+    <h1><?php esc_html_e( 'Lovetura Date', 'lovetura-date' ); ?></h1>
+    <?php var_dump($unadates); ?>
+    <form class="" action="options.php" method="post">
+    <?php settings_fields('lovedate_section'); 
+    do_settings_sections('lovedate_section'); ?>
+        <table class="form-table">
+            <tr valign="top">
+                <th scope="row">Date Format</th>
+                <!--<td><input type="text" name="lovetura_email" value="dd-mm-yy" disabled></td>-->
+                <td><label for="">dd-mm-yy</label></td>
+
+            <tr valign="top">
+                <th scope="row">Unavailable Dates</th>
+                <td>
+                <button class="button button-primary" id="addDate">+</button>
+                <div id="p_dates">
+                <?php $index = 0;
+                    foreach ( $unadates as $unadate => $date) {
+                        ?>
+                    <p>
+                    <label for="p_dats">
+                        <input type="text" name="unadates[<?php echo esc_attr($unadate); ?>]" id="" value="<?php echo esc_attr($date); ?>">
+                        <?php if ( $index > 0 ) { ?>
+                        <button class="button button-primary" id="remDate">X</button>
+                        <?php } ?>
+                    </p>
+                        <?php $index++;
+                    } ?>
+                </div>
+                </td>
+            </tr>
+            </tr>
+        </table>
+        <?php submit_button(); ?>
+    </form>
+
+</div>
